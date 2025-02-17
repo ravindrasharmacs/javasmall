320c578d4fdf60db89e5a1c5247ca3248a6501fc
ravindrasharmacs <ravindra.sharmacs@gmail.com>
2025-02-17 11:21:11 +0530

    java_026

--- sample.java
+++ sample.java
@@ -1,5 +1,10 @@
 public class sample {
     public static void main(String[] args) {
         System.out.println("Hello World");
+        System.out.println("Sum of 2 and 3 is :::"+add(2, 3));
     }
+
+    public static int add(int a, int b) {
+        return a + b;
+        }
 }
